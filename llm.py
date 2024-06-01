eb52baee093f6623c719f8a3ddceeea51e74326e
hung20gg <hung20gg@gmail.com>
2024-06-01 11:35:56 +0700

    first commit

---
+++ llm.py
@@ -0,0 +1,129 @@
+# from transformers import pipeline, AutoModelForCausalLM, AutoTokenizer, BitsAndBytesConfig
+import torch
+import gc
+import boto3
+import json
+from .llm_utils import convert_format, convert_non_system_prompts, convert_to_multimodal_format
+
+
+# class CoreLLMs:
+#     def __init__(self,
+#                 model_name = "meta-llama/Meta-Llama-3-8B-Instruct", 
+#                 quantization='auto',
+                
+#                 generation_args = None
+#                 ) -> None:
+        
+#         self.device = 'cuda' if torch.cuda.is_available() else 'cpu'
+#         self.model_name = model_name
+#         self.quantization = quantization
+#         self.is_agent_initialized = True
+#         self.tokenizer = AutoTokenizer.from_pretrained(model_name, trust_remote_code=True)
+#         self.host = 'local'
+#         self._initialize_agent()
+#         if generation_args is None:
+
+#                 self.generation_args = {
+#                     "max_new_tokens": 4096,
+#                     "temperature": 0.2,
+#                     "top_p": 0.9,
+#                 }
+#         else:
+#             self.generation_args = generation_args
+        
+#     def _initialize_agent(self):
+        
+#         quantize_config = None
+        
+#         if self.quantization == 'int4':
+#             print("Using int4")
+#             quantize_config = BitsAndBytesConfig(
+#                 load_in_4bit=True,
+#                 bnb_4bit_compute_dtype=torch.bfloat16
+#                 )
+            
+#         elif self.quantization == 'int8':
+#             print("Using int8")
+#             quantize_config = BitsAndBytesConfig(
+#                 load_in_8bit=True,
+#                 bnb_8bit_compute_dtype=torch.bfloat16
+#                 )
+
+#         self.model = AutoModelForCausalLM.from_pretrained(
+#             self.model_name, 
+#             device_map = self.device, 
+#             torch_dtype = 'auto', 
+#             trust_remote_code = True,
+#             quantization_config = quantize_config 
+#         )
+#         self.model.generation_config.pad_token_id = self.tokenizer.eos_token_id
+        
+#         self.pipe = pipeline("text-generation",
+#             model = self.model,
+#             tokenizer = self.tokenizer,
+#         )
+        
+#     def _delete_agent(self):
+#         if self.is_agent_initialized:
+#             del self.pipe
+#             del self.model
+#             gc.collect()
+#             torch.cuda.empty_cache() 
+#             self.is_agent_initialized = False
+#             print("Agent deleted")
+
+#     def __call__(self, message):
+#         if not self.is_agent_initialized:
+#             self._initialize_agent()
+#             self.is_agent_initialized = True
+#         if 'llama' not in self.model_name.lower():
+#             message = convert_non_system_prompts(message)
+#         with torch.no_grad():
+#             return self.pipe(message, **self.generation_args)[0]['generated_text']
+        
+        
+class BedRockLLMs:
+    def __init__(self,
+                model_name = "meta.llama3-8b-instruct-v1:0", 
+                access_key = None,
+                secret_key = None,
+                secret_token = None,    
+                region_name = "us-west-2"
+                 ) -> None:
+        self.client = boto3.client(service_name='bedrock-runtime', region_name=region_name, aws_access_key_id=access_key, aws_secret_access_key=secret_key, aws_session_token=secret_token)
+        self.model_id = model_name
+        self.host = 'cloud'
+        self.device = 'cuda' if torch.cuda.is_available() else 'cpu'
+        
+    
+    def _initialize_agent(self):
+        pass
+    
+    def _delete_agent(self):
+        pass
+    
+    def __call__(self, message):
+        prompt = convert_format(message)
+        if 'anthropic' in self.model_id.lower():
+            request = {
+                "anthropic_version": "bedrock-2023-05-31",
+                # Optional inference parameters:
+                "max_tokens": 4096,
+                "messages": convert_to_multimodal_format(message, has_system=False),
+            }
+            response = self.client.invoke_model(body=json.dumps(request), modelId=self.model_id, contentType='application/json')
+            return json.loads(response['body'].read().decode('utf-8'))['content'][0]['text']
+        
+        request = {
+            "prompt": prompt,
+            # Optional inference parameters:
+            "max_gen_len": 4096,
+            "temperature": 0.3,
+            "top_p": 0.9,
+        }
+        response = self.client.invoke_model(body=json.dumps(request), modelId=self.model_id, contentType='application/json')
+        return json.loads(response['body'].read().decode('utf-8'))['generation']
+    
+if __name__ == "__main__":
+    llm  = BedRockLLMs()
+    print(llm([{'role':'system','content':'You are a friendly assistant'},{'role':'user','content':'How are you today'}]))
---
+++ llm_utils.py
@@ -0,0 +1,104 @@
+import re
+import json
+import ast
+
+def get_json_from_text_response(text_response):
+    # Extract the JSON response from the text response
+    json_response = re.search(r"\[.*\]", text_response, re.DOTALL)
+    if json_response:
+        json_data = json_response.group(0)
+        try:
+            return json.loads(json_data)
+        except Exception as e1:
+            try:
+                print(e1)
+                pattern = r'\\?:\s*false\\?'
+                json_data = re.sub(pattern, ': False', json_data)
+                
+                pattern = r'\\?:\s*true\\?'
+                json_data = re.sub(pattern, ': True', json_data)
+                
+                pattern = r'\\?:\s*null\\?'
+                json_data = re.sub(pattern, ': None', json_data)
+                
+                # json_data = json_data.replace(": false,", ": False,").replace(": true,", ": True,").replace(": null,", ": None,")
+                # json_data = json_data.replace(":false,", ": False,").replace(":true,", ": True,").replace(":null,", ": None,")
+                json_data = ast.literal_eval(json_data)
+                return json.dumps(json_data, indent=4)
+            except Exception as e2:
+                print(e2)
+                print("Error in converting JSON response")
+                return text_response
+        
+        # return json.dumps(json_data)
+    print("No JSON response found in text response")
+    return text_response
+
+def convert_format(data, has_system=True):
+    prompt = ""
+    sys_non_llama = ''
+    for i, item in enumerate(data):
+        if i == 0 and item["role"] == "system":
+            if has_system:
+                system_message = item["content"]
+                prompt += f"<|begin_of_text|>\n<|start_header_id|>system<|end_header_id|>\n{system_message}<|eot_id|>\n"
+            else:
+                sys_non_llama = item["content"]+'\n'
+
+        elif item["role"] == "user":
+            if i==1 and not has_system:
+                user_message = item["content"]
+                prompt += f"<|start_header_id|>user<|end_header_id|>\n{sys_non_llama}{user_message}\n<|eot_id|>\n<|start_header_id|>assistant<|end_header_id|>\n"
+            else:
+                user_message = item["content"]
+                prompt += f"<|start_header_id|>user<|end_header_id|>\n{user_message}\n<|eot_id|>\n<|start_header_id|>assistant<|end_header_id|>\n"
+        elif item["role"] == "assistant":
+            assistant_message = item["content"]
+            prompt += f"{assistant_message}\n<|eot_id|>"
+
+    return prompt.rstrip("\n")
+
+def convert_to_multimodal_format(messages, has_system=True):
+    new_messages = []
+    if not has_system:
+        messages = convert_non_system_prompts(messages)
+    for i, item in enumerate(messages):
+        role = item["role"]
+        content = item["content"]
+        new_messages.append({"role": role, "content": [{'type':'text', 'text':content}]})
+    return new_messages
+
+def convert_non_system_prompts(messages):
+    new_messages = []
+    if messages[0]['role'] == 'system':
+        system_prompt = messages[0]['content']
+        for i in range(1,len(messages)):
+            message = messages[i]['content']
+            if i == 1:
+                message = system_prompt + '\n' + message
+            new_messages.append({"role": messages[i]['role'], "content": message})
+        return new_messages
+    return messages
+
+def flatten_conversation(messages):
+    conversation = []
+
+    for message in messages:
+        role = message['role']
+        content = message['content']
+        conversation.append(f"{role.capitalize()}: {content}")
+
+    return "\n".join(conversation)
+
+if __name__ == "__main__":
+
+    messages = [
+    {"role": "system", "content": "You are a friendly assistant"},
+    {"role": "user", "content": "What is the weather today?"},
+    {"role": "assistant", "content": "The weather today is sunny with a high of 75°F."},
+    {"role": "user", "content": "Thank you!"},
+    {"role": "assistant", "content": "You're welcome! Anything else you need?"}
+]
+
+    plain_text_conversation = flatten_conversation(messages)
+    print(plain_text_conversation)
