c6ff3c1d1d7c23281655bb317ee8b194af64f693
hung20gg <hung20gg@gmail.com>
2024-07-23 21:51:01 +0700

    4-o

--- llm.py
+++ llm.py
@@ -20,7 +20,7 @@ genai.configure(api_key=os.getenv('GENAI_API_KEY'))
 
 
 class ChatGPT:
-    def __init__(self, model_name = 'gpt-3.5-turbo', engine='davinci-codex'):
+    def __init__(self, model_name = 'gpt-4o-mini', engine='davinci-codex'):
         self.model_name = model_name
         self.engine = engine
         self.client = OpenAI(api_key=os.getenv('OPENAI_API_KEY'))
