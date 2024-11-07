c51bca5370ea84f8fb3d25eb0b63ee14c84724e1
Quang Hưng <110990347+hung20gg@users.noreply.github.com>
2024-11-07 12:22:42 +0700

    usage

--- llm/abstract.py
+++ llm/abstract.py
@@ -11,3 +11,9 @@ class LLM(BaseModel):
     def reset_token(self):
         self.input_token = 0
         self.output_token = 0
+        
+    def usage(self):
+        return {
+            'input_token': self.input_token,
+            'output_token': self.output_token
+        }
