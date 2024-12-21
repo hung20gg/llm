f38ca74bdff873d2fb738cc9b7f618c4ef9f099a
Quang Hưng <110990347+hung20gg@users.noreply.github.com>
2024-12-21 18:26:34 +0700

    abstract

--- llm/abstract.py
+++ llm/abstract.py
@@ -6,6 +6,15 @@ class LLM:
     def __call__(self, message, **kwargs) -> str:
         raise NotImplementedError()
     
+    def stream(self, message, **kwargs):
+        raise NotImplementedError()
+    
+    def batch_call(self, messages, **kwargs):
+        raise NotImplementedError()
+    
+    def retrieve(self):
+        raise NotImplementedError()
+    
     def reset_token(self):
         self.input_token = 0
         self.output_token = 0
